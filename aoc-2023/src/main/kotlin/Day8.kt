57ecf7d16a935e046596d3a4ead26583b9213d8d
Sergey Kuznetsov <sergey.kuznetsov@infobip.com>
2023-12-08 10:41:45 +0100

    Move lcm to utils

--- aoc-2023/src/main/kotlin/Day8.kt
+++ aoc-2023/src/main/kotlin/Day8.kt
@@ -55,23 +55,8 @@ class Day8(
             count
         }
 
-        return trips.reduce { acc, i -> findLCM(acc, i) }
+        return trips.lcm()
     }
-
-    private fun findLCM(a: Long, b: Long): Long {
-        val larger = if (a > b) a else b
-        val maxLcm = a * b
-        var lcm = larger
-
-        while (lcm <= maxLcm) {
-            if (lcm % a == 0L && lcm % b == 0L) {
-                return lcm
-            }
-            lcm += larger
-        }
-        return maxLcm
-    }
-
 }
 
 fun main() {
--- aoc-2023/src/main/kotlin/util/Utils.kt
+++ aoc-2023/src/main/kotlin/util/Utils.kt
@@ -1,3 +1,19 @@
 fun String.splitToNumbers(): Sequence<Long> = this.splitToSequence(" ").mapNotNull(::mapToLong)
 
 fun mapToLong(s: String): Long? = s.trim().takeIf { it.isNotBlank() }?.toLong()
+
+fun Iterable<Long>.lcm(): Long = this.reduce { acc, l -> lcm(acc, l) }
+
+private fun lcm(a: Long, b: Long): Long {
+    val larger = if (a > b) a else b
+    val maxLcm = a * b
+    var lcm = larger
+
+    while (lcm <= maxLcm) {
+        if (lcm % a == 0L && lcm % b == 0L) {
+            return lcm
+        }
+        lcm += larger
+    }
+    return maxLcm
+}
