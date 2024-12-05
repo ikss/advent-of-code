7fa6759c6504d16b1bab2f709eddb2f81b00d4d2
Sergey Kuznetsov <sergey.kuznetsov@infobip.com>
2024-12-05 07:01:25 +0100

    Day 6 preparation

---
+++ aoc-2024/src/main/kotlin/Day6.kt
@@ -0,0 +1,26 @@
+class Day6 {
+    private val input = run {
+        this::class.java.getResourceAsStream(this::class.simpleName!!.lowercase() + ".txt")!!.bufferedReader().readLines()
+    }
+
+    fun part1(): Long {
+        var result = 0L
+
+
+        return result
+    }
+
+    fun part2(): Long {
+        var result = 0L
+
+
+        return result
+    }
+}
+
+fun main() {
+    val day = Day6()
+    println("part1: " + day.part1()) // 
+    println("part2: " + day.part2()) // 
+}
+            
