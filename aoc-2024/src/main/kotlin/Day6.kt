b74d92d9065ff60fdb72060cf44b16cf2b0cdc62
Sergey Kuznetsov <sergey.kuznetsov@infobip.com>
2024-12-06 20:46:38 +0100

    Day 6 optimized

--- aoc-2024/src/main/kotlin/Day6.kt
+++ aoc-2024/src/main/kotlin/Day6.kt
@@ -1,3 +1,5 @@
+import com.google.common.base.Stopwatch
+
 class Day6 : DayX() {
     private val grid = input.toCharGrid()
     private val start = input.findStart('^')
@@ -71,6 +73,73 @@ class Day6 : DayX() {
 
         return result
     }
+
+    fun part2optimized(): Long {
+        var result = 0L
+        var curr = start to Direction.UP
+        val visited = HashSet<Pair<Point, Direction>>()
+        val placedObstacles = HashSet<Point>()
+
+        while (true) {
+            val (point, direction) = curr
+            visited.add(curr)
+
+            val (nextr, nextc) = point + direction.next
+
+            if (nextr !in grid.indices || nextc !in grid[nextr].indices) {
+                break
+            }
+            curr = if (grid[nextr][nextc] != '#') {
+                val nextPoint = nextr to nextc
+                if (nextPoint !in placedObstacles && start != nextPoint) {
+                    placedObstacles.add(nextPoint)
+                    
+                    val old = grid[nextr][nextc]
+                    grid[nextr][nextc] = '#'
+                    
+                    if (hasCycle(point, direction.getRight(), visited)) {
+                        result++
+                    }
+                    grid[nextr][nextc] = old
+                }
+                nextPoint to direction
+            } else {
+                point to direction.getRight()
+            }
+        }
+
+        return result
+    }
+
+    private fun hasCycle(
+        startPoint: Point,
+        startDirection: Direction,
+        visitedBefore: java.util.HashSet<Pair<Point, Direction>>,
+    ): Boolean {
+        val visited = HashSet(visitedBefore)
+        var curr = startPoint to startDirection
+
+        while (true) {
+            val (point, direction) = curr
+
+            if (visited.contains(point to direction)) {
+                return true
+            }
+            visited.add(point to direction)
+
+            val (nextr, nextc) = point + direction.next
+
+            if (nextr !in grid.indices || nextc !in grid[0].indices) {
+                break
+            }
+            curr = if (grid[nextr][nextc] != '#') {
+                nextr to nextc to direction
+            } else {
+                point to direction.getRight()
+            }
+        }
+        return false
+    }
 }
 
 fun main() {
@@ -78,5 +147,8 @@ fun main() {
     day.solve()
     // Part 1: 4883
     // Part 2: 1655
+    val sw = Stopwatch.createStarted()
+    println(day.part2optimized())
+    println(sw.stop())
 }
             
