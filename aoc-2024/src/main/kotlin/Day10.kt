17aff78aaf0d2aa9738d3bfed41c6e0e2a2bd0e8
Sergey Kuznetsov <sergey.kuznetsov@infobip.com>
2024-12-10 07:19:12 +0100

    Day 10: Hoof It

--- aoc-2024/src/main/kotlin/Day10.kt
+++ aoc-2024/src/main/kotlin/Day10.kt
@@ -1,21 +1,45 @@
 class Day10 : DayX() {
+    private val grid = input.toCharGrid()
+    private val starts = input.findStarts('0')
+    private val trails = starts.map { getTrails(it) }
 
     override fun part1(): Long {
-        var result = 0L
-
-        return result
+        return trails.sumOf { it.toSet().size }.toLong()
     }
 
     override fun part2(): Long {
-        var result = 0L
+        return trails.sumOf { it.size }.toLong()
+    }
+
+    private fun getTrails(start: Pair<Int, Int>): List<Point> {
+        val queue = java.util.ArrayDeque<Pair<Point, Char>>()
+        queue.offer(start to '0')
+        val result = ArrayList<Point>()
+
+        while (queue.isNotEmpty()) {
+            val (point, char) = queue.poll()
+            if (char == '9') {
+                result.add(point)
+                continue
+            }
+            val newChar = char + 1
+
+            for (dir in fourDirections) {
+                val (nr, nc) = point + dir
+                if (nr in grid.indices && nc in grid[0].indices && grid[nr][nc] == newChar) {
+                    queue.offer(Point(nr, nc) to newChar)
+                }
+            }
+        }
 
         return result
     }
+
 }
 
 fun main() {
     val day = Day10()
     day.solve()
-    // Part 1: 
-    // Part 2: 
+    // Part 1: 825
+    // Part 2: 1805
 }
--- shared/src/main/kotlin/InputUtils.kt
+++ shared/src/main/kotlin/InputUtils.kt
@@ -13,6 +13,20 @@ fun List<String>.findStart(vararg startChars: Char): Point {
     throw IllegalArgumentException("No start found")
 }
 
+
+fun List<String>.findStarts(vararg startChars: Char): List<Point> {
+    val result = ArrayList<Point>()
+    for (r in this.indices) {
+        for (c in this[r].indices) {
+            if (this[r][c] in startChars) {
+                result.add(Point(r, c))
+            }
+        }
+    }
+    if (result.isEmpty()) throw IllegalArgumentException("No start found")
+    return result
+}
+
 fun mapToLong(s: String): Long? = s.trim().takeIf { it.isNotBlank() }?.toLong()
 
 fun String.splitToNumbers(delimiter: Char = ' '): Sequence<Long> = this.splitToSequence(delimiter).mapNotNull(::mapToLong)
