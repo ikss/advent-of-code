1296e10f3912f886d40db82d40a4118f6d6706bb
Sergey Kuznetsov <sergey.kuznetsov@infobip.com>
2024-12-11 18:59:13 +0100

    Day 12 preparation

---
+++ aoc-2024/src/main/kotlin/Day12.kt
@@ -0,0 +1,21 @@
+class Day12 : DayX() {
+
+    override fun part1(): Long {
+        var result = 0L
+
+        return result
+    }
+
+    override fun part2(): Long {
+        var result = 0L
+
+        return result
+    }
+}
+
+fun main() {
+    val day = Day12()
+    day.solve()
+    // Part 1: 
+    // Part 2: 
+}
