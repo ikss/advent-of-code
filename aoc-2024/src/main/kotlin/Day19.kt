69b348e403ced89bf006d16cabdd20c41ecb0490
Sergey Kuznetsov <sergey.kuznetsov@infobip.com>
2024-12-19 07:43:09 +0100

    Day 19: Linen Layout

--- aoc-2024/src/main/kotlin/Day19.kt
+++ aoc-2024/src/main/kotlin/Day19.kt
@@ -1,20 +1,46 @@
 class Day19 : DayX() {
 
     override fun part1(): Long {
+        val towels = input[0].split(", ")
 
+        val patterns = input.takeLastWhile { it.isNotEmpty() }
 
-        return -1L
+        return patterns.count { canBreak(it, towels) > 0L }.toLong()
     }
 
     override fun part2(): Long {
+        val towels = input[0].split(", ")
 
-        return -1L
+        val patterns = input.takeLastWhile { it.isNotEmpty() }
+
+        return patterns.sumOf { canBreak(it, towels) }
+    }
+
+    private fun canBreak(p: String, towels: List<String>): Long {
+        val dp = LongArray(p.length)
+
+        for (i in p.indices) {
+            for (t in towels) {
+                if (i < t.length - 1) {
+                    continue
+                }
+                if (i == t.length - 1) {
+                    if (p.substring(0, i + 1) == t) {
+                        dp[i] += 1L
+                    }
+                } else if (dp[i - t.length] > 0 && p.substring(i - t.length + 1, i + 1) == t) {
+                    dp[i] += dp[i - t.length]
+                }
+            }
+        }
+
+        return dp[p.length - 1]
     }
 }
 
 fun main() {
     val day = Day19()
     day.solve()
-    // Part 1: 
-    // Part 2: 
+    // Part 1: 278
+    // Part 2: 569808947758890
 }
