e48bd09eb45a90a35850fb02ca388fca8f7044fd
Sergey Kuznetsov <sergey.kuznetsov@infobip.com>
2024-12-26 08:58:13 +0100

    Day 25: Code Chronicle

--- aoc-2024/src/main/kotlin/Day25.kt
+++ aoc-2024/src/main/kotlin/Day25.kt
@@ -1,14 +1,37 @@
 class Day25 : DayX() {
 
     override fun part1(): Any {
+        val blocks = input.joinToString("\n").split("\n\n").map { it.split("\n") }
+
+        val locks = blocks.filter { it[0].all { it == '#' } }
+        val keys = blocks.filter { it[0].all { it == '.' } }
+        
         var result = 0L
+        
+        for (key in keys) {
+            for (lock in locks) {
+                var intersect = false
+                for (ki in 0 until key.size) {
+                    if (intersect) break
+                    for (j in 0 until key[ki].length) {
+                        if (key[ki][j] == '#' && lock[ki][j] == '#') {
+                            intersect = true
+                            break
+                        }
+                    }
+                }
+                if (!intersect) {
+                    result++
+                }
+            }
+        }
 
         return result
     }
 
     override fun part2(): Any {
         var result = 0L
-        
+
         return result
     }
 }
@@ -16,6 +39,6 @@ class Day25 : DayX() {
 fun main() {
     val day = Day25()
     day.solve()
-    // Part 1: 
+    // Part 1: 3196
     // Part 2: 
 }
