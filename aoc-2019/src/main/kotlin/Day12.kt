34958136d061a2f15b82f51182b80c06d83660db
Sergey Kuznetsov <sergey.kuznetsov@infobip.com>
2025-01-09 17:46:01 +0100

    2019: Day 12 preparation

---
+++ aoc-2019/src/main/kotlin/Day12.kt
@@ -0,0 +1,24 @@
+import IntcodeComputer.Companion.POISON_PILL
+import kotlin.concurrent.thread
+
+class Day12(title: String) : DayX(title) {
+    private val numbers = input.readAllNumbers().toList()
+
+    override fun part1(): Long {
+
+        return -1L
+    }
+
+
+    override fun part2(): Long {
+        
+        return -1L
+    }
+}
+
+fun main() {
+    val day = Day12("Day 12: The N-Body Problem")
+    day.solve()
+    // Part 1: 
+    // Part 2: 
+}
