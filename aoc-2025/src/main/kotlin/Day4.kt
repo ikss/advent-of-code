e6fa33c564b2ed5f2f43e6c1c38845c3fb277406
Sergey Kuznetsov <sergey.kuznetsov@infobip.com>
2025-12-03 08:51:33 +0100

    2025: Day 4 preparation

---
+++ aoc-2025/src/main/kotlin/Day4.kt
@@ -0,0 +1,23 @@
+class Day4(title: String) : DayX(title) {
+
+    override fun part1(): Any {
+        var result = 0L
+
+        return result
+    }
+
+    override fun part2(): Any {
+        var result = 0L
+
+        return result
+    }
+}
+
+fun main() {
+    val day = Day4("Day 4: ")
+    day.solve()
+    // Example part 1:
+    // Example part 2:
+    // Real part 1:
+    // Real part 2:
+}
