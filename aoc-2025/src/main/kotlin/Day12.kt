38b839a12bf74640a87b725b650c6613495cbd22
Sergey Kuznetsov <sergey.kuznetsov@infobip.com>
2025-12-12 10:35:27 +0100

    2025: Day 12: Christmas Tree Farm

--- aoc-2025/src/main/kotlin/Day12.kt
+++ aoc-2025/src/main/kotlin/Day12.kt
@@ -3,6 +3,22 @@ class Day12(title: String) : DayX(title) {
     override fun part1(): Any {
         var result = 0L
 
+        var areas = IntArray(6)
+        for (i in 0 until 6) {
+            for (l in 1..3) {
+                areas[i] += input[i * 5 + l].count { it == '#' }
+            }
+        }
+
+        for (l in input.drop(30)) {
+            val (areanum, nums) = l.split(": ")
+            val (x, y) = areanum.split("x")
+            val area = x.toLong() * y.toLong()
+            val need = nums.split(" ").map { it.toInt() }.withIndex().sumOf { areas[it.index].toLong() * it.value }
+            if (need <= area) {
+                result++
+            }
+        }
 
         return result
     }
@@ -15,10 +31,8 @@ class Day12(title: String) : DayX(title) {
 }
 
 fun main() {
-    val day = Day12("Day 12")
+    val day = Day12("Day 12: Christmas Tree Farm")
     day.solve()
-    // Example part 1:
-    // Example part 2:
-    // Real part 1:
-    // Real part 2:
+    // Example part 1: 2
+    // Real part 1: 433
 }
